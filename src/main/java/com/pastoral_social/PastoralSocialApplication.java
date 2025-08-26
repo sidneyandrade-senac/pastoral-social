9d842f4c0788180d61cea13ed15e080902c7ee9d
SidneyAltoQi <sidney.andrade@altoqi.com.br>
2025-08-26 16:21:36 -0300

    chore(backend): criação do projeto Spring Boot com Maven

---
+++ src/main/java/com/pastoral_social/PastoralSocialApplication.java
@@ -0,0 +1,13 @@
+package com.pastoral_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastoralSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastoralSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pastoral_social/PastoralSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastoral_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastoralSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
